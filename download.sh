87f62ff9122ef0c513278dcbecf7c92059f1b9d9
Sebastian Ksiazczyk <acq19sk@croco.dcs.shef.ac.uk>
2020-08-06 10:35:05 +0100

    download script complete

--- download.sh
+++ download.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+lgs=($1 $2)
 src=$1
 tgt=$2
+IFS=$'\n' lgs=($(sort <<<"${lgs[*]}"))
+unset IFS
 
-wget "http://opus.nlpl.eu/download.php?f=OpenSubtitles/v2018/xml/$src.zip" -O "OpenSubtitles/xml/$src.zip"
-wget "http://opus.nlpl.eu/download.php?f=OpenSubtitles/v2018/xml/$tgt.zip" -O "OpenSubtitles/xml/$tgt.zip"
-wget "http://opus.nlpl.eu/download.php?f=OpenSubtitles/v2018/xml/$src-$tgt.xml.gz" -O "OpenSubtitles/$src-$tgt.xml.gz"
+echo ${lgs[@]}
+
+mkdir -p OpenSubtitles
+mkdir -p OpenSubtitles/xml
+
+wget "http://opus.nlpl.eu/download.php?f=OpenSubtitles/v2018/xml/$src.zip" -O ./OpenSubtitles/xml/"$src".zip
+wget "http://opus.nlpl.eu/download.php?f=OpenSubtitles/v2018/xml/$tgt.zip" -O ./OpenSubtitles/xml/"$tgt".zip
+wget "http://opus.nlpl.eu/download.php?f=OpenSubtitles/v2018/xml/${lgs[0]}-${lgs[1]}.xml.gz" -O ./OpenSubtitles/"${lgs[0]}"-"${lgs[1]}".xml.gz
 
 unzip OpenSubtitles/xml/"$src".zip
 unzip OpenSubtitles/xml/"$tgt".zip
 
-gzip OpenSubtitles/"$src"-"$tgt".xml.gz
+gunzip OpenSubtitles/"${lgs[0]}"-"${lgs[1]}".xml.gz
