b4e19b4c885cd308ad6d086f6f5a1312ad341890
xiazczyk <acq19sk@croco.dcs.shef.ac.uk>
2020-08-07 11:29:32 +0100

    clearing up the paths

--- download.sh
+++ download.sh
@@ -7,15 +7,19 @@ IFS=$'\n' lgs=($(sort <<<"${lgs[*]}"))
 unset IFS
 
 echo ${lgs[@]}
+pairname="${lgs[0]}-${lgs[1]}"
+echo $pairname
 
 mkdir -p OpenSubtitles
 mkdir -p OpenSubtitles/xml
-mkdir -p OpenSubtitles/parsed
+mkdir -p OpenSubtitles/"$pairname"/parsed
+mkdir -p OpenSubtitles/"$pairname"/cxt_dataset
+
 wget "http://opus.nlpl.eu/download.php?f=OpenSubtitles/v2018/xml/$src.zip" -O ./OpenSubtitles/xml/"$src".zip
 wget "http://opus.nlpl.eu/download.php?f=OpenSubtitles/v2018/xml/$tgt.zip" -O ./OpenSubtitles/xml/"$tgt".zip
-wget "http://opus.nlpl.eu/download.php?f=OpenSubtitles/v2018/xml/${lgs[0]}-${lgs[1]}.xml.gz" -O ./OpenSubtitles/"${lgs[0]}"-"${lgs[1]}".xml.gz
+wget "http://opus.nlpl.eu/download.php?f=OpenSubtitles/v2018/xml/$pairname.xml.gz" -O ./OpenSubtitles/"$pairname"/"$pairname".xml.gz
 
 unzip OpenSubtitles/xml/"$src".zip
 unzip OpenSubtitles/xml/"$tgt".zip
 
-gunzip OpenSubtitles/"${lgs[0]}"-"${lgs[1]}".xml.gz
+gunzip OpenSubtitles/"$pairname"/"$pairname".xml.gz
